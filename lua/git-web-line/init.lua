082f061abcdf9657198b3c618df52763c332b55a
Robbie R <git@robbie.computer>
2023-11-10 23:43:54 +0000

    Lint with stylua

--- lua/git-web-line/init.lua
+++ lua/git-web-line/init.lua
@@ -14,14 +14,17 @@ end
 function M.git_remote_host()
   -- gets the domain name of the remote host repo
   -- e.g. github.com
-  local remote_host = vim.fn.system("git config --get remote.origin.url | sed -n 's/.*@//;s/:.*//p' | tr -d '\n'")
+  local remote_host =
+    vim.fn.system("git config --get remote.origin.url | sed -n 's/.*@//;s/:.*//p' | tr -d '\n'")
   return remote_host
 end
 
 function M.git_remote_repo()
   -- gets the name of the remote repo
   -- e.g. dotfiles
-  local remote_repo_name = vim.fn.system("git config --get remote.origin.url | sed -n 's/.*\\///;s/.git.*//p' | tr -d '\n'")
+  local remote_repo_name = vim.fn.system(
+    "git config --get remote.origin.url | sed -n 's/.*\\///;s/.git.*//p' | tr -d '\n'"
+  )
   -- local remote_repo_name = vim.fn.system("git config --get remote.origin.url | sed -n 's/.*://;s/.git.*//p' | tr -d '\n'")
   return remote_repo_name
 end
@@ -29,13 +32,14 @@ end
 function M.git_remote_username()
   -- gets the username from the remote repo
   -- e.g. torvalds
-  local remote_repo_username = vim.fn.system("git config --get remote.origin.url | sed -n 's/.*://;s/\\/.*//p' | tr -d '\n'")
+  local remote_repo_username =
+    vim.fn.system("git config --get remote.origin.url | sed -n 's/.*://;s/\\/.*//p' | tr -d '\n'")
   return remote_repo_username
 end
 
 function M.current_filepath()
   -- gets the relative path of the open file
-  local filepath = vim.fn.expand("%")
+  local filepath = vim.fn.expand('%')
   return filepath
 end
 
@@ -48,12 +52,23 @@ function M.activate()
   local remote_repo_username = M.git_remote_username()
   local file_path = M.current_filepath()
 
-  local url = "https://" .. remote_host .. "/" .. remote_repo_username .. "/" .. remote_repo_name .. "/blob/" .. branch_name .. "/" .. file_path .. "#L" .. current_line
+  local url = 'https://'
+    .. remote_host
+    .. '/'
+    .. remote_repo_username
+    .. '/'
+    .. remote_repo_name
+    .. '/blob/'
+    .. branch_name
+    .. '/'
+    .. file_path
+    .. '#L'
+    .. current_line
 
-  print("Opening " .. url)
+  print('Opening ' .. url)
 
   -- open the url in the browser
-  vim.fn.system("open " .. url)
+  vim.fn.system('open ' .. url)
 end
 
 return M
