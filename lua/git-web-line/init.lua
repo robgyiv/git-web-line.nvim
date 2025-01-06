c4a27df8acf61f355334d9a01fc8f630ba3078fe
Robbie R <git@robbie.computer>
2025-01-06 14:44:19 +0800

    Simplify control flow, supporting both protocols

--- lua/git-web-line/init.lua
+++ lua/git-web-line/init.lua
@@ -8,53 +8,72 @@ local function git_branch_name()
   return vim.fn.system("git branch --show-current | tr -d '\n'")
 end
 
-local function _is_ssh_remote(git_remote_string)
-  return string.match(git_remote_string, 'git@')
+local function _is_ssh_remote(git_remote_str)
+  return git_remote_str:match('^git@') and true or false
 end
 
-local function git_remote_host(git_remote_string)
-  if _is_ssh_remote(git_remote_host(git_remote_string)) then
-    return vim.fn.system(
-      'echo ' .. git_remote_string .. " | sed -n 's/.*@//;s/:.*//p' | tr -d '\n'"
-    )
-  end
-  -- return domain for https remotes
-  return vim.fn.system(
-    'echo ' .. git_remote_string .. " | sed -n 's/.*\\/\\///;s/\\/.*//p' | tr -d '\n'"
-  )
-end
-
-local function git_repo_path(git_remote_string)
-  return vim.fn.system(
-    'echo ' .. git_remote_string .. " | sed -e 's/^[^:]*:[^/]*\\///' -e 's/\\.git$//' | tr -d '\n'"
-  )
-end
-
-local function git_remote_username(git_remote_string)
-  return vim.fn.system(
-    'echo ' .. git_remote_string .. " | sed -n 's/.*://;s/\\/.*//p' | tr -d '\n'"
-  )
+local function _is_https_remote(git_remote_str)
+  return git_remote_str:match('^https://') and true or false
 end
 
 local function current_filepath()
   return vim.fn.expand('%:~:.')
 end
 
+local function detect_protocol(git_remote_str)
+  if _is_ssh_remote(git_remote_str) then
+    return 'ssh'
+  elseif _is_https_remote(git_remote_str) then
+    return 'https'
+  else
+    return nil
+  end
+end
+
+local function parse_https(git_remote_str)
+  local domain, username, name = git_remote_str:match('https://([^/]+)/([^/]+)/(.+)%.git$')
+  return {
+    protocol = 'https',
+    domain = domain,
+    username = username,
+    name = name,
+  }
+end
+
+local function parse_ssh(git_remote_str)
+  local domain, username, name = git_remote_str:match('git@([^:]+):([^/]+)/(.+)%.git$')
+  return {
+    protocol = 'ssh',
+    domain = domain,
+    username = username,
+    name = name,
+  }
+end
+
+local function parse_remote(git_remote_str)
+  local protocol = detect_protocol(git_remote_str)
+  if protocol == 'https' then
+    return parse_https(git_remote_str)
+  elseif protocol == 'ssh' then
+    return parse_ssh(git_remote_str)
+  else
+    error('Unsupported protocol: ' .. (git_remote_str or 'nil'))
+  end
+end
+
 function M.activate()
-  local git_remote_string = vim.fn.system("git remote get-url origin | tr -d '\n'")
+  local git_remote_str = vim.fn.system("git remote get-url origin | tr -d '\n'")
   local current_line = current_line_number()
-  local branch_name = git_branch_name()
-  local remote_host = git_remote_host(git_remote_string)
-  local repo_path = git_repo_path(git_remote_string)
-  local remote_repo_username = git_remote_username(git_remote_string)
   local file_path = current_filepath()
+  local branch_name = git_branch_name()
+  local repository = parse_remote(git_remote_str)
 
   local url = 'https://'
-    .. remote_host
+    .. repository.domain
     .. '/'
-    .. remote_repo_username
+    .. repository.username
     .. '/'
-    .. repo_path
+    .. repository.name
     .. '/blob/'
     .. branch_name
     .. '/'
